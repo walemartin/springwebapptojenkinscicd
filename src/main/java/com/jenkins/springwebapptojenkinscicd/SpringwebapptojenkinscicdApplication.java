34046d6b58cca15731f8bc1c6aa5d745d6bee66d
Olawale Olasupo <rytesoft@hotmail.com>
2023-05-09 16:19:22 +0200

    first commit

---
+++ src/main/java/com/jenkins/springwebapptojenkinscicd/SpringwebapptojenkinscicdApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.springwebapptojenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebapptojenkinscicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebapptojenkinscicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/springwebapptojenkinscicd/SpringwebapptojenkinscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.springwebapptojenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebapptojenkinscicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
